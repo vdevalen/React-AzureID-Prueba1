46b5880dd29adf50c6bddf48c400eae1a977a5c6
Valentina Pinzón Montoya <valenpinzonn@gmail.com>
2024-02-12 00:48:01 -0500

    prueba 1

---
+++ Config.js
@@ -0,0 +1,8 @@
+export const config = {
+    appId: 'c3744f00-57a4-465c-bcf2-57bca50f6750',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+        'user.read'
+    ],
+    authority: 'https://login.microsoftonline.com/f03c8212-e84c-4e71-970d-c13d7c6b6b9d/v2.0'
+};
---
+++ src/Config.js
@@ -0,0 +1,8 @@
+export const config = {
+    appId: 'f03c8212-e84c-4e71-970d-c13d7c6b6b9d',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+        'user.read'
+    ],
+    authority: 'https://login.microsoftonline.com/mariologika.onmicrosoft.com'
+};
